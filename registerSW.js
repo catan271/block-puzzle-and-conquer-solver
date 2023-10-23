2a1b1c6029d969e2f516a328952bc5e1f5597d14
catan271 <canh.tranduc2002@gmail.com>
2023-10-24 03:01:20 +0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/block-puzzle-and-conquer-solver/sw.js', { scope: '/block-puzzle-and-conquer-solver/' })})}
